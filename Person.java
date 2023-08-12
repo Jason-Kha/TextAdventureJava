e3a3167325c5074af0f29f2b1cb13e1d78fa7adb
Jason Kha <jasonkha63@yahoo.com>
2023-08-12 15:28:53 -0500

    Added plans

---
+++ Person.java
@@ -0,0 +1,10 @@
+package TextAdventureJava;
+
+public class Player {
+    private String name;
+    private int level;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
